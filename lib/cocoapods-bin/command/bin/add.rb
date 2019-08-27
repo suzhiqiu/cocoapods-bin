e79dfdad9a107621f5ba01b3aa7dbeae0d154f43
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-27 16:51:29 +0800

    no waring

--- lib/cocoapods-bin/command/bin/add.rb
+++ lib/cocoapods-bin/command/bin/add.rb
@@ -1,10 +1,12 @@
+require 'cocoapods'
+
 module Pod
   class Command
     class Bin < Command
       class Add < Bin
         self.summary = '再不删除二进制的情况下为组件添加源码调试能力,多个组件名称用空格分隔'
 
- 
+
 
         def initialize(argv)
           @update = argv.flag?('update')
@@ -14,26 +16,19 @@ module Pod
         end
 
         def run
-          update_if_necessary!
 
-          source = @code ? code_source : binary_source
-
-          sets = if @diff 
-                  binary_pod_set_names = binary_source.pod_sets.map(&:name)
-                  sets = code_source.pod_sets.reject { |set| binary_pod_set_names.include?(set.name) }
-                else
-                  source.pod_sets
-                end
-
-          sets.each { |set| UI.pod(set, :name_and_version) }
-          UI.puts "\n#{sets.count} pods were found"
+          command = 'dwarfdump /Users/suzhiqiu/Library/Developer/Xcode/DerivedData/LibSource-gvmdthzquecjhpdskogxxrkgtmhc/Build/Products/Debug-iphonesimulator/libLibSource.a | grep \'DW_AT_comp_dir\''
+          UI.puts "#{command}".red
+          output = `#{command}`.lines.to_a
+          UI.puts "#{output}".red
+         # if output.empty
+          #  UI.puts "没找到二进制编译路径信息".red
+         # end
+         # https://github.com/suzhiqiu/UCARRobot.git
+         # command = 'git clone  https://github.com/suzhiqiu/UCARRobot  ~/Downloads/'
+         # output = `#{command}`.lines.to_a
         end
 
-        def update_if_necessary!
-          UI.section("\nUpdating Spec Repositories\n".yellow) do
-            Pod::Command::Bin::Repo::Update.new(CLAide::ARGV.new([])).run 
-          end if @update 
-        end
       end
     end
   end
