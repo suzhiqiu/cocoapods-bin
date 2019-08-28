0dc43c5b45558cdb29ef22eb6982c91f2e4fb286
suzhiqiu <zhiqiu.su@ucarinc.com>
2019-08-28 14:29:35 +0800

    update

--- lib/cocoapods-bin/command/bin/add.rb
+++ lib/cocoapods-bin/command/bin/add.rb
@@ -1,68 +1,50 @@
-require 'cocoapods'
 
 module Pod
   class Command
     class Bin < Command
       class Add < Bin
-        self.summary = '再不删除二进制的情况下为组件添加源码调试能力,多个组件名称用空格分隔'
+        self.summary = '再不删除二进制的情况下为组件添加源码调试能力,多个组件名称用逗号分隔'
 
+     #   def initialize(argv)
+      #    @nameArgv = argv.shifint_argument
+     #     UI.puts "add输入参数:#{@nameArgv}".red
+      #    super
+      #  end
 
-
-        def initialize(argv)
-          @update = argv.flag?('update')
-          @code  = argv.flag?('code')
-          @diff  = argv.flag?('diff')
-          super
+        def run        
+          addSource
         end
 
-        def run
+        def addSource()
+          nameArray = @nameArgv.split
+          if nameArray.length == 0
+           UI.puts "请输入要下载组件源码名称".red
+           return 
+          end
+          nameArray.each_index {|index|
+           item = nameArray[index]
+           UI.puts "#{item}".red
+           downSource(item)
+         }
+         UI.puts "下载完成...".red
 
+        end
+
+        def downSource(name)
           command = 'dwarfdump /Users/suzhiqiu/Library/Developer/Xcode/DerivedData/LibSource-gvmdthzquecjhpdskogxxrkgtmhc/Build/Products/Debug-iphonesimulator/libLibSource.a | grep \'DW_AT_comp_dir\''
-          UI.puts "#{command}".red
-          output = `#{command}`.lines.to_a
-          UI.puts "#{output}".red
+  
+          UI.puts "#{command}".red    
          # if output.empty
           #  UI.puts "没找到二进制编译路径信息".red
          # end
-          UI.puts "开始下载...".red
-          command = 'git clone  https://github.com/suzhiqiu/UCARRobot  ~/Downloads/q'
+          serverPath = 'https://github.com/suzhiqiu/UCARRobot'
+          localPath = '~/Downloads/q'
+
+          UI.puts "开始下载#{name}".red
+          command = `git clone  #{serverPath}  #{localPath}`
           output = `#{command}`.lines.to_a
-          UI.puts "完成...".red
-          list
-          delete
+          UI.puts "#{output}".red
         end
-
-        def list()
-          UI.puts "开始统计大小...".red
-          # @path = ''
-          command = 'du -sh  /Users/suzhiqiu/Desktop/bin/lib/* | sort -n'
-          output = `#{command}`
-          UI.puts "#{output}"
-          UI.puts "结束统计大小...".red
-        end
-
-        def list()
-          UI.puts "开始统计大小...".red
-          # @path = ''
-          command = 'du -sh  /Users/suzhiqiu/Desktop/bin/lib/* | sort -n'
-          output = `#{command}`
-          UI.puts "#{output}"
-          UI.puts "结束统计大小...".red
-        end
-
-        def delete()
-          UI.puts "开始删除...".red
-          # @path = ''
-          command = 'rm -rf  /Users/suzhiqiu/Downloads/q'
-          output = `#{command}`
-          UI.puts "#{output}"
-          UI.puts "结束删除...".red
-        end
-
-
-
-
-
 
       end
     end
--- lib/cocoapods-bin/command/bin/clean.rb
+++ lib/cocoapods-bin/command/bin/clean.rb
@@ -3,43 +3,21 @@ module Pod
     class Bin < Command
       class Clean < Bin
         self.summary = '删除所有已经下载的源码'
-
-        def self.options
-          [
-            ['--code',  '展示源码 pods'],
-            ['--diff',  '展示缺少二进制版本的源码 pods'],
-            ['--update', '展示前执行 `pod bin repo update`'],
-          ].concat(super)
-        end
-
-        def initialize(argv)
-          @update = argv.flag?('update')
-          @code  = argv.flag?('code')
-          @diff  = argv.flag?('diff')
-          super
-        end
+        self.arguments = []
 
         def run
-          update_if_necessary!
-
-          source = @code ? code_source : binary_source
-
-          sets = if @diff 
-                  binary_pod_set_names = binary_source.pod_sets.map(&:name)
-                  sets = code_source.pod_sets.reject { |set| binary_pod_set_names.include?(set.name) }
-                else
-                  source.pod_sets
-                end
-
-          sets.each { |set| UI.pod(set, :name_and_version) }
-          UI.puts "\n#{sets.count} pods were found"
+         @path = '/Users/suzhiqiu/Downloads/q'
+         delete(path)
         end
-
-        def update_if_necessary!
-          UI.section("\nUpdating Spec Repositories\n".yellow) do
-            Pod::Command::Bin::Repo::Update.new(CLAide::ARGV.new([])).run 
-          end if @update 
+        
+        def delete(path)
+            UI.puts "#{path}".red
+            UI.puts "开始删除中,请稍候...".red
+            # @path = ''
+            command = `rm -rf  #{path}`
+            UI.puts "结束删除...".red
         end
+   
       end
     end
   end
